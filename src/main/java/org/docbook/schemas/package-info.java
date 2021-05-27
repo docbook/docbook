6b60dfe142d52eb160cfb6a559e74a8bcbbf4466
Norman Walsh <ndw@nwalsh.com>
2021-05-27 15:14:25 +0100

    Configuration and resources for the docbook-schemas Maven artifact

---
+++ src/main/java/org/docbook/schemas/package-info.java
@@ -0,0 +1,7 @@
+/** The DocBook schemas.
+ *
+ * <p>This package contains the DocBook schema files. They can
+ * be accessed by unzipping the jar file or, in some environments,
+ * simply using <code>classpath:</code> URIs.</p>
+ */
+package org.docbook.schemas;
